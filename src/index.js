de6b6a6450d915b4608fef9d7559ebb544e4c456
Shadowborn <james.starks023@gmail.com>
2019-10-22 00:56:53 -0600

    repository change

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ^ this is taking the document and replacing the elment with the id of root in our html file, with our function App
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
